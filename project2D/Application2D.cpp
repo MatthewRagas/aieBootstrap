7d17fde831a794ba49658a5d3263faea5192b305
Matthew Ragas <mjrag310@gmail.com>
2017-11-28 16:34:14 -0600

    Application files

--- project2D/Application2D.cpp
+++ project2D/Application2D.cpp
@@ -23,8 +23,10 @@ bool Application2D::startup() {
 	m_cameraX = 0;
 	m_cameraY = 0;
 	m_timer = 0;
-	mPx = 0;
-	mPy = 0;
+	mPx = 750;
+	mPy = 450;
+
+	mPlayer = new Player(5, 5, new Vector2(mPx, mPy));
 
 	return true;
 }
@@ -45,33 +47,29 @@ void Application2D::update(float deltaTime) {
 	aie::Input* input = aie::Input::getInstance();
 
 	// use arrow keys to move camera
-	if (input->isKeyDown(aie::INPUT_KEY_UP))
-		m_cameraY += 500.0f * deltaTime;
+	//if (input->isKeyDown(aie::INPUT_KEY_W))
+	//	m_cameraY += 550.0f * deltaTime;
 
-	if (input->isKeyDown(aie::INPUT_KEY_DOWN))
-		m_cameraY -= 500.0f * deltaTime;
+	//if (input->isKeyDown(aie::INPUT_KEY_S))
+	//	m_cameraY -= 550.0f * deltaTime;
 
-	if (input->isKeyDown(aie::INPUT_KEY_LEFT))
-		m_cameraX -= 500.0f * deltaTime;
+	//if (input->isKeyDown(aie::INPUT_KEY_A))
+	//	m_cameraX -= 550.0f * deltaTime;
 
-	if (input->isKeyDown(aie::INPUT_KEY_RIGHT))
-		m_cameraX += 500.0f * deltaTime;
+	//if (input->isKeyDown(aie::INPUT_KEY_D))
+	//	m_cameraX += 550.0f * deltaTime;
 	
-	// use WASD to move box
-	if (input->isKeyDown(aie::INPUT_KEY_W))
-		mPy += 480.0f * deltaTime;
-	
-	if (input->isKeyDown(aie::INPUT_KEY_S))
-		mPy -= 480.0f * deltaTime;
+	// use WASD to move Player
+	mPlayer->MovePlayer(deltaTime, input);
 
-	
+	//Player take damage
+	if (input->isKeyDown(aie::INPUT_KEY_Q))
+		mPlayer->TakeDamage(10);
 
-	if (input->isKeyDown(aie::INPUT_KEY_A))
-		mPx -= 480.0f * deltaTime;
-
-	if (input->isKeyDown(aie::INPUT_KEY_D))
-		mPx += 480.0f * deltaTime;
-
+	//Game Over condition
+	if (mPlayer->GetHealth() <= 0);
+		draw();
+		
 	// exit the application
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
 		quit();
@@ -88,38 +86,43 @@ void Application2D::draw() {
 	// begin drawing sprites
 	m_2dRenderer->begin();
 
+	//Game Over Text
+	if (mPlayer->GetHealth() <= 0)
+		mPlayer->GameOver();
+
 	// demonstrate animation
-	m_2dRenderer->setUVRect(int(m_timer) % 8 / 8.0f, 0, 1.f / 8, 1.f / 8);
-	m_2dRenderer->drawSprite(m_texture, 200, 200, 100, 100);
+	/*m_2dRenderer->setUVRect(int(m_timer) % 8 / 8.0f, 0, 1.f / 8, 1.f / 8);
+	m_2dRenderer->drawSprite(m_texture, 200, 200, 100, 100);*/
 
 	// demonstrate spinning sprite
-	m_2dRenderer->setUVRect(0,0,1,1);
-	m_2dRenderer->drawSprite(m_shipTexture, 600, 400, 0, 0, m_timer, 1);
+	/*m_2dRenderer->setUVRect(0,0,1,1);
+	m_2dRenderer->drawSprite(m_shipTexture, 600, 400, 0, 0, m_timer, 1);*/
 
 	// draw a thin line
-	m_2dRenderer->drawLine(300, 300, 600, 400, 2, 1);
+	//m_2dRenderer->drawLine(300, 300, 600, 400, 2, 1);//
 
 	// draw a moving purple circle
-	m_2dRenderer->setRenderColour(1, 0, 1, 1);
-	m_2dRenderer->drawCircle(sin(m_timer) * 100 + 600, 150, 50);
+	/*m_2dRenderer->setRenderColour(1, 0, 1, 1);
+	m_2dRenderer->drawCircle(sin(m_timer) * 100 + 600, 150, 50);*/
 
 	// draw a rotating red box
 	m_2dRenderer->setRenderColour(1, 0, 0, 1);
 	m_2dRenderer->drawBox(600, 500, 60, 20, m_timer);
 
 	// draw a slightly rotated sprite with no texture, coloured yellow
-	m_2dRenderer->setRenderColour(1, 1, 0, 1);
-	m_2dRenderer->drawSprite(nullptr, 400, 400, 50, 50, 3.14159f * 0.25f, 1);
+	/*m_2dRenderer->setRenderColour(1, 1, 0, 1);
+	m_2dRenderer->drawSprite(nullptr, 400, 400, 50, 50, 3.14159f * 0.25f, 1);*/
 	
-	// a box
-	m_2dRenderer->drawBox(mPx, mPy, 80, 80, 0);
-	
-
 	// output some text, uses the last used colour
 	char fps[32];
 	sprintf_s(fps, 32, "FPS: %i", getFPS());
-	m_2dRenderer->drawText(m_font, fps, 0, 720 - 32);
-	m_2dRenderer->drawText(m_font, "Press ESC to quit!", 0, 720 - 64);
+	m_2dRenderer->drawText(m_font, fps, 0, 850 - 32);
+	m_2dRenderer->drawText(m_font, "Arcade Zombies", 0, 850);
+	m_2dRenderer->drawText(m_font, "Press esc to quit", 1200, 850);
+
+	// a box
+	m_2dRenderer->setRenderColour(1, 1, 1, 1);
+	m_2dRenderer->drawSprite(m_shipTexture, mPlayer->Position().mX , mPlayer->Position().mY, 80, 80, 0);
 
 	// done drawing sprites
 	m_2dRenderer->end();
