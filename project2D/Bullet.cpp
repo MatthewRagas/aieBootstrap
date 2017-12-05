5a199e8018f530f5226f5cb3f66709fbb0118f0f
Matthew Ragas <mjrag310@gmail.com>
2017-12-05 17:39:38 -0600

    Bullet files

--- project2D/Bullet.cpp
+++ project2D/Bullet.cpp
@@ -1,4 +1,5 @@
 #include "Bullet.h"
+#include "Input.h"
 
 Bullet::Bullet()
 {
@@ -10,11 +11,22 @@ Bullet::~Bullet()
 
 Bullet::Bullet(Vector2 *position, int movespeed, int damage)
 {
-	mPosition = position;
+	mPosition = new Vector2(position->mX, position->mY);
 	mMovespeed = movespeed;
 	mDamage = damage;
+	mIsFired = false;
 }
 
-void Bullet::FireBullet(float deltaTime, aie::Input * input)
+void Bullet::FireBullet(float deltaTime)
 {
+	if (!mIsFired)
+		return;
+
+	*mPosition = *mPosition + *mDirection * mMovespeed * deltaTime;
+}
+
+void Bullet::Fire(Vector2 dir)
+{
+	mIsFired = true;
+	mDirection = new Vector2(dir.mX, dir.mY);
 }
--- project2D/Bullet.h
+++ project2D/Bullet.h
@@ -6,10 +6,13 @@ class Bullet
 {
 public:
 	Vector2 *mPosition;
+	Vector2 *mDirection;
+	bool mIsFired;
 	int mMovespeed;
 	int mDamage;
 	Bullet();
 	~Bullet();
 	Bullet(Vector2 *position, int movespeed, int damage);
-	void FireBullet(float deltaTime, aie::Input *input);
+	void FireBullet(float deltaTime);
+	void Fire(Vector2 dir);
 };
